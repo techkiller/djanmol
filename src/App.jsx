e915f2a0e8f61c70c7c1eaa7571e767e8fd6fae9
techkiller <pankajjaangir@gmail.com>
2026-01-30 15:57:11 +0530

    new

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <marquee behavior="" direction="">Under Development.. Launching soon</marquee>
+    </>
+  )
+}
+
+export default App
